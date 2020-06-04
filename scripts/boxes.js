d5509158ad9be0b158fc231d86a63adf8c55ad75
Darryl Cousins <darryljcousins@gmail.com>
2020-06-05 08:09:25 +1200

    First baby steps

---
+++ scripts/boxes.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom"
+import React from "react"
+import { Boxes } from "./Components/Boxes"
+
+const rootEl = document.getElementById("react-boxes")
+
+rootEl && ReactDOM.render(, rootEl)
